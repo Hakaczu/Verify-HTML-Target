1d96a93bdafecaaeb3cbe5931b969f40fd4b4e90
Sebastian Szypulski <32041785+Sisa49@users.noreply.github.com>
2019-01-13 18:20:31 +0100

    Add Description

--- verifyTarget.js
+++ verifyTarget.js
@@ -1,3 +1,4 @@
+//set HTML rel attribute when HTML target attribute is not "_blank" on all a HTML elements in file
 function verifyTarget(){
     var elements = document.getElementsByTagName("a");
     for (let index = 0; index < elements.length; index++) {
