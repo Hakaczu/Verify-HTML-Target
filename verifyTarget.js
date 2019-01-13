a96babc55583268b719a5ff7a939279d56b63927
Sebastian Szypulski <32041785+Sisa49@users.noreply.github.com>
2019-01-13 18:37:21 +0100

    update functions

--- verifyTarget.js
+++ verifyTarget.js
@@ -1,10 +1,11 @@
-//set HTML rel attribute when HTML target attribute is not "_blank" on all a HTML elements in file
+//set HTML rel attribute when HTML target attribute is "_blank" on all a HTML elements in file
 function verifyTarget(){
     var elements = document.getElementsByTagName("a");
     for (let index = 0; index < elements.length; index++) {
         const element = elements[index];
-        if(element.getAttribute("target") != "_blank"){
+        if(element.getAttribute("target") == "_blank"){
             element.setAttribute("rel", "noopener noreferrer");
         }
     }
 }
+window.onload = verifyTarget();
